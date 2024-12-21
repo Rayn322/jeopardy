21cf2794e16a9945a1448bab3dadd7472c21c69d
Ryan Lauderbach <16494880+Rayn322@users.noreply.github.com>
2024-12-21 00:53:16 -0800

    Partying

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,25 @@
 <script lang="ts">
+	import { PUBLIC_PARTYKIT_HOST } from '$env/static/public';
+	import PartySocket from 'partysocket';
 	import '../app.css';
 	let { children } = $props();
+
+	const ws = new PartySocket({
+		host: PUBLIC_PARTYKIT_HOST,
+		room: 'test'
+	});
+
+	let lastMessage = $state('no message');
+
+	ws.onmessage = (event) => {
+		lastMessage = event.data;
+	};
 </script>
 
 {@render children()}
+
+<p>{lastMessage}</p>
+
+<button class="rounded border-2 border-black p-4" onclick={() => ws.send('hello')}
+	>Send message</button
+>
